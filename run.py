a83924c61ab0dd79101e09d4ec9b0a46e97e23b9
Jae Chang <jchang@AF051591.employee-wifi.rwc.corp.imvu.com>
2017-09-06 08:00:56 -0700

    Fully working AWS, Postgresql RDS.

---
+++ run.py
@@ -0,0 +1,18 @@
+
+import os
+from myapp import create_app
+
+app = create_app(os.environ.get('FLASK_CONFIG', 'development'))
+
+if __name__ == '__main__':
+    # with app.app_context():
+        #db.create_all()
+        # # create a development user
+        # if User.query.get(1) is None:
+        #     u = User(username='john')
+        #     u.set_password('cat')
+        #     db.session.add(u)
+        #     db.session.commit()
+    app.run(debug=True, host='0.0.0.0')
+    
+
